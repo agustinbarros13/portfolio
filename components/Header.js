76f26630c243ae976020a6ec27b8c7cf3df52c95
agustinbarros13 <agustin-b_@outlook.es>
2024-07-18 12:47:37 +0200

    cambios generales responsive

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,31 @@
 import './Header.css'
 
-export default function Header() {
+function Header() {
   const header = document.createElement('header')
-  header.innerHTML = `
-    <nav>
-      <h1>Agustín Barros</h1>
-      <ul>
-        <li><a href="#about">Sobre Mí</a></li>
-        <li><a href="#projects">Proyectos</a></li>
-      </ul>
-    </nav>
-  `
+  const nav = document.createElement('nav')
+  const h1 = document.createElement('h1')
+  h1.textContent = 'Agustín Barros'
+
+  const ul = document.createElement('ul')
+  const aboutLi = document.createElement('li')
+  const aboutLink = document.createElement('a')
+  aboutLink.href = '#about'
+  aboutLink.textContent = 'Sobre Mí'
+
+  const projectsLi = document.createElement('li')
+  const projectsLink = document.createElement('a')
+  projectsLink.href = '#projects'
+  projectsLink.textContent = 'Proyectos'
+
+  aboutLi.appendChild(aboutLink)
+  projectsLi.appendChild(projectsLink)
+  ul.appendChild(aboutLi)
+  ul.appendChild(projectsLi)
+  nav.appendChild(h1)
+  nav.appendChild(ul)
+  header.appendChild(nav)
+
   return header
 }
+
+export default Header
--- components/Projects.js
+++ components/Projects.js
@@ -1,21 +1,48 @@
 import './Projects.css'
+import project1Image from '../image/proyecto1.jpg'
+import project2Image from '../image/proyecto2.jpg'
 
-export default function Projects() {
-  const projects = document.createElement('section')
-  projects.innerHTML = `
-    <h2>Proyectos</h2>
-    <div class="project-list">
-      <div class="project">
-        <h3>Proyecto 1</h3>
-        <p> Es un proyecto de The power, donde se trabaja con JS y Vite. 
-         Este es el link:  <a href="https://github.com/agustinbarros13/entrega-vite-js" target="_blank">https://github.com/agustinbarros13/entrega-vite-js</a>
-        </p>
+const projectsData = [
+  {
+    title: 'Proyecto 1',
+    description: 'Es un proyecto de The power, donde se trabaja con JS y Vite.',
+    link: 'https://github.com/agustinbarros13/entrega-vite-js',
+    image: project1Image
+  },
+  {
+    title: 'Proyecto 2',
+    description:
+      'Mantenimiento y desarrollo de una Web sencilla. (CSS, JS y HTML)',
+    link: 'https://www.evfilms.es/productora_de_video_en_valladolid',
+    image: project2Image
+  }
+]
+
+function Projects() {
+  const projectsSection = document.createElement('section')
+  projectsSection.setAttribute('id', 'projects')
+
+  const projectList = document.createElement('div')
+  projectList.classList.add('project-list')
+
+  projectsData.forEach((project) => {
+    const projectDiv = document.createElement('div')
+    projectDiv.classList.add('project')
+
+    projectDiv.innerHTML = `
+      <h3>${project.title}</h3>
+      <div class="project-content">
+        <img src="${project.image}" alt="${project.title}" class="project-image">
+        <p>${project.description} Link: <a href="${project.link}" target="_blank">${project.link}</a></p>
       </div>
-      <div class="project">
-        <h3>Proyecto 2</h3>
-        <p>Mantenimiento y desarrollo de una Web sencilla.(CSS, JS y HTML) Link:<a href="https://www.evfilms.es/productora_de_video_en_valladolid" target="_blank">https://www.evfilms.es/productora_de_video_en_valladolid</a> </p>
-      </div>
-    </div>
-  `
-  return projects
+    `
+
+    projectList.appendChild(projectDiv)
+  })
+
+  projectsSection.appendChild(projectList)
+
+  return projectsSection
 }
+
+export default Projects
